6fe924d3bd8287439f70329aefd13d989425de8d
Patrick Tierney <patrick.l.tierney@gmail.com>
2009-07-09 01:04:14 +0000

    Hello world

---
+++ include/kaleidoscope/kaleidoscope.h
@@ -0,0 +1,28 @@
+
+
+#pragma once
+
+#include <iostream>
+
+
+
+namespace Kaleidoscope {
+	
+	class Kaleidoscope{
+		
+	public:
+		Kaleidoscope( int argc, char** argv ){
+			
+			std::cout << "Hello world" << std::endl;
+
+		}
+	
+		void run(){
+
+
+		}
+	};
+
+}
+
+namespace Kal = Kaleidoscope;
