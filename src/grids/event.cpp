eec609e87238d9f345c8ca8894ef57125cb886b8
Patrick Tierney <patrick.l.tierney@gmail.com>
2009-07-09 06:19:20 +0000

    Wrote test_event

---
+++ src/grids/event.cpp
@@ -0,0 +1,47 @@
+
+#include <string>
+#include <vector>
+#include <iostream>
+
+#include <grids/event.h>
+#include <grids/define.h>
+
+namespace Grids
+{
+	Event::Event( ) {
+		event_type = GRIDS_NULL_EVENT;
+	}
+
+	Event::Event( std::string in_event ){
+		event_type = in_event;
+	}
+
+	Event::Event( std::string in_event, Value value )	{
+		event_type = in_event;
+		args = value;
+	}
+
+	void Event::setEvent(std::string in_event)	{
+		event_type = in_event;
+	}
+
+	void Event::setArgs( Value value)	{
+		args = value;
+	}
+
+	Grids::Value Event::getArgs()	{
+		return args;
+	}
+
+	Grids::Value * Event::getArgsPtr( )	{
+		return &args;
+	}
+
+	std::string Event::getEventType()	{
+		return event_type;
+	}
+
+
+
+}
+
---
+++ src/grids/test_event.cpp
@@ -0,0 +1,67 @@
+
+
+#include <grids/event.h>
+
+#include <gtest/gtest.h>
+
+namespace{
+
+	class EventTest : public ::testing::Test {
+
+	protected:			
+		virtual void SetUp(){
+			test_val[ "foo" ][ "bar" ][ "lol" ][ "dongs" ] = 111u;
+			test_event_type = "Lol.Dongs";
+		}
+
+		std::string test_event_type;
+		Grids::Value test_val;
+	};
+		
+	TEST_F(EventTest, ConstructorTest){
+		Grids::Event temp_evt;
+		Grids::Value temp_val = temp_evt.getArgs();
+				
+		EXPECT_STREQ( GRIDS_NULL_EVENT, temp_evt.getEventType().c_str() );
+		EXPECT_TRUE( temp_evt.getArgs().empty() );		
+		EXPECT_TRUE( temp_evt.getArgsPtr()->empty() );		
+		EXPECT_NE( NULL, (unsigned int)temp_evt.getArgsPtr() );
+		
+		temp_evt = Grids::Event( test_event_type );
+		
+		EXPECT_STREQ( test_event_type.c_str(), temp_evt.getEventType().c_str() );
+		EXPECT_TRUE( temp_evt.getArgs().empty() );		
+		EXPECT_TRUE( temp_evt.getArgsPtr()->empty() );		
+		EXPECT_NE( NULL, (unsigned int)temp_evt.getArgsPtr() );
+		
+		temp_evt = Grids::Event( test_event_type, test_val );
+		
+		EXPECT_STREQ( test_event_type.c_str(), temp_evt.getEventType().c_str() );
+		EXPECT_FALSE( temp_evt.getArgs().empty() );		
+		EXPECT_FALSE( temp_evt.getArgsPtr()->empty() );		
+		EXPECT_NE( NULL, (unsigned int)temp_evt.getArgsPtr() );
+		
+		EXPECT_EQ( 111u, temp_evt.getArgs()[ "foo" ][ "bar" ][ "lol" ][ "dongs" ].asUInt() );
+		EXPECT_EQ( 111u, (*(temp_evt.getArgsPtr()))[ "foo" ][ "bar" ][ "lol" ][ "dongs" ].asUInt() );
+		
+	}
+
+	TEST_F(EventTest, getSetEventTypeTest){
+		Grids::Event temp_evt;
+		std::string temp_string = "foo.bar";
+
+		temp_evt.setEvent( temp_string );
+
+		EXPECT_STREQ( temp_string.c_str(), temp_evt.getEventType().c_str() );
+
+	}
+
+	TEST_F(EventTest, getSetArgsTest) {
+		Grids::Event temp_evt;
+		
+		temp_evt.setArgs( test_val );
+		
+		EXPECT_EQ( 111u, temp_evt.getArgs()[ "foo" ][ "bar" ][ "lol" ][ "dongs" ].asUInt() );
+	}
+
+}
