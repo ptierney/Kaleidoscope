7394caf330be22da295ea3a46a9df0b569ae793b
Patrick Tierney <patrick.l.tierney@gmail.com>
2010-04-20 17:37:42 +0000

    4.20 presentation

--- Qt/Kaleidoscope/ui_kaleidoscope.h
+++ Qt/Kaleidoscope/ui_kaleidoscope.h
@@ -1,7 +1,7 @@
 /********************************************************************************
 ** Form generated from reading UI file 'kaleidoscope.ui'
 **
-** Created: Tue Apr 20 00:18:40 2010
+** Created: Tue Apr 20 13:32:25 2010
 **      by: Qt User Interface Compiler version 4.6.2
 **
 ** WARNING! All changes made in this file will be lost when recompiling UI file!
